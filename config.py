b821086392ffde84c708b5b3273c78caac05320c
Carlos Chinchilla <calufa@gmail.com>
2019-09-18 16:37:28 -0700

    add “ROTATION_ANGLE” and “SCALED_VIDEO_RESOLUTION” to config

--- config.py
+++ config.py
@@ -7,5 +7,7 @@ MOUTH_LMS_FILE_PATH = '/data/mouth-landmarks.json'
 OFFSET_END = 0.1
 ORIGINAL_VIDEO_RESOLUTION = (1080, 1920) # width, height
 PERCENTAGE_CLIP = 3
+ROTATION_ANGLE = 90
+SCALED_VIDEO_RESOLUTION = (512, 1024) # width, height
 SILENT_IPA_CODE = 'P'
 TEXT_FILE_PATH = '/tmp/transcript.txt'
