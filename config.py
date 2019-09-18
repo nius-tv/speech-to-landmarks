c064a8a7db5c13dbfae2a8d161fc112a02746e05
Carlos Chinchilla <calufa@gmail.com>
2019-09-18 15:53:45 -0700

    add IMG_FMT and ORIGINAL_VIDEO_RESOLUTION to config

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 DEFAULT_MOUTH_IPA_CODE = 'AA'
 FORCED_ALIGNER_URL = 'http://gentle:8765/transcriptions?async=false'
 FPS = 30
+IMG_FMT = 'png'
 MIN_PERCENTAGE = 0.7
 MOUTH_LMS_FILE_PATH = '/data/mouth-landmarks.json'
 OFFSET_END = 0.1
+ORIGINAL_VIDEO_RESOLUTION = (1080, 1920) # width, height
 PERCENTAGE_CLIP = 3
 SILENT_IPA_CODE = 'P'
 TEXT_FILE_PATH = '/tmp/transcript.txt'
