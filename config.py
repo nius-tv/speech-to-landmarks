b210b512de5a49c301e58c0cd20353e276fae58a
Carlos Chinchilla <calufa@gmail.com>
2019-10-02 17:20:43 -0500

    expose INPUT_TEXT env var

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
+import os
+
 AUDIO_FILE_PATH = '/data/audio.wav'
 DEFAULT_MOUTH_IPA_CODE = 'AA'
 FORCED_ALIGNER_URL = 'http://gentle:80/transcriptions?async=false'
 FPS = 30
 IMG_FMT = 'png'
-INPUT_TEXT = 'wave glow is awesome'
+INPUT_TEXT = os.environ.get('INPUT_TEXT')
 MIN_PERCENTAGE = 0.7
 MOUTH_LMS_FILE_PATH = '/models/mouth-landmarks.json'
 MOUTH_LMS_INFERENCE_DIR_PATH = '/data/inference'
